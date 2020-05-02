12de14a1182dd56891084b017e5926f24a36020d
Declan Cox <declan.cox@picomass.com>
2020-05-02 17:06:28 +0100

    Enhanced example with scheduler calling bash script which calls another bash script.

--- eg_daemon.py
+++ eg_daemon.py
@@ -1,62 +1,120 @@
-#!/usr/bin/env python3.5
-import sys
-import os
-import time
+#!/usr/bin/env python3
 import argparse
+import functools
 import logging
+import os
+import subprocess
+import sys
+import threading
+import time
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+import schedule
+
 import daemon
 from daemon import pidfile
 
-debug_p = False
+debug_p = True
 
-def do_something(logf):
-    ### This does the "work" of the daemon
+HOME = os.environ["HOME"]
 
-    logger = logging.getLogger('eg_daemon')
-    logger.setLevel(logging.INFO)
+working_dir = Path("/home/dc/eg_daemon")
+eg_pidfile = working_dir / "eg_daemon.pid"
+eg_logfile = working_dir / "eg_daemon.log"
 
-    fh = logging.FileHandler(logf)
-    fh.setLevel(logging.INFO)
 
-    formatstr = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(formatstr)
+def start_daemon():
+    print(f"eg_daemon: entered start_daemon()")
+    print(f"eg_daemon: pid = {eg_pidfile} log = {eg_logfile}")
+    print(f"eg_daemon: about to daemonize...")
 
-    fh.setFormatter(formatter)
+    fh = get_file_handler(eg_logfile, FORMATTER)
+    logger = get_logger("eg_daemon", fh)
 
-    logger.addHandler(fh)
+    dctx = daemon.DaemonContext(
+        working_directory=working_dir,
+        stdout=fh.stream,
+        stderr=fh.stream,
+        umask=0,
+        pidfile=pidfile.TimeoutPIDLockFile(eg_pidfile),
+    )
 
+    print(f"working_dir={dctx.working_directory}\nuid={dctx.uid}\ngid={dctx.gid}\nchroot={dctx.chroot_directory}")
+    print(f"preserved={dctx.files_preserve}\nstdout={dctx.stdout}\n")
+    print(f"signal_map={dctx.signal_map}\n")
+
+    with dctx as context:
+        do_something(logger)
+
+
+def do_something(logger):
+    """
+    This is the actual process doing the work
+    :param logf:
+    :return:
+    """
+
+    provisioning_thread = threading.Thread(target=run_script_periodically, args=(logger,))
+    provisioning_thread.daemon = True
+    provisioning_thread.start()
+
+    # print(f"starting scheduler: {provisioning_thread.is_alive()}")
+
+    #
     while True:
-        logger.debug("this is a DEBUG message")
-        logger.info("this is an INFO message")
-        logger.error("this is an ERROR message")
-        time.sleep(5)
+        logger.debug("doing something..")
+        time.sleep(10)
 
 
-def start_daemon(pidf, logf):
-    ### This launches the daemon in its context
+def run_script_periodically(logger):
+    logger.debug("scheduling periodic script...")
+    threading.current_thread().name = 'provisioning'
+    schedule.every(5).seconds.do(functools.partial(run_script, logger)).tag("myscript")
+    while True:
+        schedule.run_pending()
 
-    global debug_p
 
-    if debug_p:
-        print("eg_daemon: entered run()")
-        print("eg_daemon: pidf = {}    logf = {}".format(pidf, logf))
-        print("eg_daemon: about to start daemonization")
+def run_script(logger):
+    logger.debug("running script..")
 
-    ### XXX pidfile is a context
-    with daemon.DaemonContext(
-        working_directory='/var/lib/eg_daemon',
-        umask=0o002,
-        pidfile=pidfile.TimeoutPIDLockFile(pidf),
-        ) as context:
-        do_something(logf)
+    e = subprocess.run(
+        ["/home/dc/eg_daemon/my_script.bash"],
+        capture_output=False,
+        cwd=working_dir
+    )
+
+    if e.returncode != 0:
+        # Something went wrong with the script execution, can't really recover from this
+        logger.error(
+            f"{e}: returned non zero exit status")
+
+    logger.debug(f"return code = {e.returncode}")
+    return e.returncode
+
+
+FORMATTER = logging.Formatter(
+    "%(asctime)-15s %(levelname)-8s: %(filename)-10s:%(funcName)-30s:%(lineno)3i : [%(threadName)-12s]: %(message)s")
+
+
+def get_file_handler(logfile, formatter):
+    file_handler = RotatingFileHandler(
+        logfile,
+        mode='a',
+        maxBytes=10 * 1024 * 1024,
+        backupCount=2,
+        encoding=None)
+    file_handler.setFormatter(formatter)
+    return file_handler
+
+
+def get_logger(logger_name, file_handler):
+    _logger = logging.getLogger(logger_name)
+    _logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+    _logger.addHandler(file_handler)
+    # with this pattern, it's rarely necessary to propagate the error up to parent
+    _logger.propagate = False
+    return _logger
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Example daemon in Python")
-    parser.add_argument('-p', '--pid-file', default='/var/run/eg_daemon.pid')
-    parser.add_argument('-l', '--log-file', default='/var/log/eg_daemon.log')
-
-    args = parser.parse_args()
-    
-    start_daemon(pidf=args.pid_file, logf=args.log_file)
-
+    start_daemon()
